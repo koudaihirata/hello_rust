0457bd7919ac8c390eaf09f60797a32826d37e7a
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 12:36:44 +0900

    ループ文

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 fn main() {
-    let x = 1;
-    match x {
-        0 => println!("Zero!"),
-        1 => {
-            println!("one");
-            println!("one");
-        },
-        _ => println!("Other"),
-    };
+    // loop :無限ループを発生させる
+    // let mut cnt = 0;
+    // loop {
+    //     println!("Hello");
+    //     if cnt == 10 {
+    //         break;
+    //     }
+    //     cnt += 1;
+    // }
 
-    let y = match x {
-        0 => 0,
-        1 => 10,
-        _ => 100
-    };
-    println!("{:?}", y);
+    // while
+    // let mut cnt = 0;
+    // while cnt <= 10 {
+    //     println!("Hello");
+    //     cnt += 1;
+    // }
+
+    // for
+    for i in [1, 2, 3] {
+        println!("Hello, {}", i);
+    }
+
+    let r = 1..=10;
+    for x in r {
+        println!("{}", x * x);
+    }
 }
