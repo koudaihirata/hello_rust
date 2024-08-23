5b92cb0218f037a82c5b46d6eea600f784a77db9
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 12:06:18 +0900

    ブロック

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-fn say_hello() {
-    println!("Hello");
-}
-
-fn add(a: i32,b: i32) -> i32 {
-    a + b
-}
-
 fn main() {
-    say_hello();
-    println!("{}", add(1, 2));
+    println!("a");
+    {
+        println!("b");
+    }
+    println!("c");
 
-    let c = add(3, 4);
-    // println!("{}", c);
-    let d = say_hello();
-    // println!("{:?}", d);
+    // シャドーイング
+    let y = 10;
+    println!("{}", y);
+    {
+        let y = 5;
+        println!("{}", y);
+    }
+    println!("{}", y);
+
+    let z = {
+        100
+    };
+    println!("{}", z);
 }
