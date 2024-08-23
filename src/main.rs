7dc105f16dd4a240f69d150dd04f07afecb818ce
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 12:25:37 +0900

    match文

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 fn main() {
-    let x = 30;
-
-    if x > 0 {
-        println!("OK!");
-    }
-
-    if x > 0 && x < 10 {
-        println!("0 < x and x < 10");
-    }
-    if x > 0 || x < 10 {
-        println!("0 < x or x < 10");
-    }
-
-    if x > 0 && x <= 10 {
-        println!("first condition");
-    } else if x > 11 && x <= 20 {
-        println!("second condition");
-    } else {
-        println!("else");
-    }
-
-    let y = if x > 10 {
-        x
-    } else {
-        "0" // 型が違うのでエラーになる
+    let x = 1;
+    match x {
+        0 => println!("Zero!"),
+        1 => {
+            println!("one");
+            println!("one");
+        },
+        _ => println!("Other"),
     };
+
+    let y = match x {
+        0 => 0,
+        1 => 10,
+        _ => 100
+    };
+    println!("{:?}", y);
 }
