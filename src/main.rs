4d98712686957759ac6855d0953fa8f557781df5
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 11:15:22 +0900

    配列

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::f64;
-
 fn main() {
-    let t1: (i32, bool, f64) = (1, true, 2.0);
+    let l1 = [1, 2, 3];
+    let l2 = [0; 1000];
 
-    println!("{:?}",t1);
+    println!("{:?}", l1);
 
-    let i = t1.0;
-    println!("{}",i);
+    let i: i32 = l1[0];
 
-    // let (x: f64, y: i32, _) = t2;
+    let [x, y, z] = l1;
 
-    // let u = ();
+    let l3: &[i32] = &l1[..];
+    println!("{:?}", l3);
 }
