286dedb4bcd6df48a9927e84910bffc143984685
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 10:59:27 +0900

    タプル

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
+use std::f64;
+
 fn main() {
-    // 数値型
-    let a = 1;
-    let b = 2.0;
+    let t1: (i32, bool, f64) = (1, true, 2.0);
 
-    let c: u16 = 3;
+    println!("{:?}",t1);
 
-    let d: f32 = 4.0f32;
+    let i = t1.0;
+    println!("{}",i);
 
-    let e: i32 = 1 + 2; // 足し算
-    let e: i32 = 1 - 2; // 引き算
-    let e: i32 = 1 * 2; // 掛け算
-    let e: i32 = 1 / 2; // 割り算
-    let e: i32 = 1 % 2; // 余り
+    // let (x: f64, y: i32, _) = t2;
 
-    let f: f64 = 1 as f64 + 2.0;
-
-    // 論理型
-    // true, false
-    let g: bool = false;
-
-    let h: bool = 1 == 2; // 等価比較
-    let h: bool = 1 != 2; // 非等価比較
-    let h: bool = 1 > 2; // 大なり
-    let h: bool = 1 < 2; // 小なり
-    let h: bool = 1 >= 2; // 大なりイコール
-    let h: bool = 1 <= 2; // 小なりイコール
+    // let u = ();
 }
