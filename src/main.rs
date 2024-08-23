55b1b92d3623230ee0f7f67fc005ae28d5ac463e
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 16:42:34 +0900

    スマートポインタ

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,17 @@
-// fn fizzbuzz(end: i32) {
-//     let mut x = 1;
-
-//     while x <= end {
-//         if x % 3 == 0 && x % 5 == 0 {
-//             println!("FizzBuzz");
-//         } else if x % 3 == 0 {
-//             println!("Fizz");
-//         } else if x % 5 == 0 {
-//             println!("Buzz");
-//         } else {
-//             println!("{}", x);
-//         }
-//         x += 1;
-//     }
-// }
-
-// fn fizzbuzz(end: i32) {
-//     let r = 1..=end;
-
-//     for x in r {
-//         match x % 15 {
-//             0 => println!("FizzBuzz"),
-//             3 | 6 | 9 | 12 => println!("Fizz"),
-//             5 | 10 => println!("Buzz"),
-//             _ => println!("{}", x)
-//         }
-//     }
-// }
-
-fn fizzbuzz(end: i32) {
-    let r = 1..=end;
-
-    for x in r {
-        match (x % 3, x % 5) {
-            (0, 0) => println!("FizzBuzz"),
-            (0, _) => println!("Fizz"),
-            (_, 0) => println!("Buzz"),
-            _ => println!("{}", x)
-        }
-    }
-}
-
+use std::rc::Rc;
 
 fn main() {
-    fizzbuzz(30);
+    let x = Box::new(1);
+    println!("x: {:p}", x);
+    println!("x + 2 = {}", *x + 2);
+
+    let a = Rc::new("Hello".to_string());
+    println!("count1: {}", Rc::strong_count(&a));
+    {
+        let b = Rc::clone(&a);
+        println!("a: {:p}", a);
+        println!("b: {:p}", b);
+        println!("count2: {}", Rc::strong_count(&a));
+    }
+    println!("count3: {}", Rc::strong_count(&a));
 }
