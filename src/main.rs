2ec4d88156dbd38c53537c1f3f5c26f47529c0bf
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 15:26:01 +0900

    matchのパターンをタプルに置き換え

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,27 @@
 //     }
 // }
 
+// fn fizzbuzz(end: i32) {
+//     let r = 1..=end;
+
+//     for x in r {
+//         match x % 15 {
+//             0 => println!("FizzBuzz"),
+//             3 | 6 | 9 | 12 => println!("Fizz"),
+//             5 | 10 => println!("Buzz"),
+//             _ => println!("{}", x)
+//         }
+//     }
+// }
+
 fn fizzbuzz(end: i32) {
     let r = 1..=end;
 
     for x in r {
-        match x % 15 {
-            0 => println!("FizzBuzz"),
-            3 | 6 | 9 | 12 => println!("Fizz"),
-            5 | 10 => println!("Buzz"),
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
             _ => println!("{}", x)
         }
     }
