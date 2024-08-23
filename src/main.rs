9b333a7ea94019d23c10b49592012247ae24ec28
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 18:46:56 +0900

    マッチガード

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ fn main() {
     //     Some(T),
     // }
 
-    let a = Some(1);
-    let b = Some("str");
-    let c: Option<i32> = None;
+    // let a = Some(1);
+    // let b = Some("str");
+    // let c: Option<i32> = None;
 
     let v = vec![1, 2, 3];
-    let val = v.get(2);
+    let val = v.get(0);
 
     match val {
+        Some(x) if *x == 1 => println!("value is 1"),
         Some(x) => println!("value exists: {}",x),
         None => println!("value is None"),
     }
 
-    if let Some(x) = val {
-        println!("val={}", x);
-    }
+    // if let Some(x) = val {
+    //     println!("val={}", x);
+    // }
 }
