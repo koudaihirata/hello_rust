465fbfb78f937aef61e20dd35b1f909e66ea56ad
koudaihirata <2230051@ecc.ac.jp>
2024-08-23 17:06:40 +0900

    型関連関数

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn new(width: u32, height: u32) -> Self {
+        Rectangle {width, height}
+    }
 }
 
 fn main() {
-    let mut rectangle: Rectangle = Rectangle {
-        width: 10,
-        height: 5,
-    };
+    let mut rectangle: Rectangle = Rectangle::new(10, 10);
     println!("width: {}", rectangle.width);
     println!("height: {}", rectangle.height);
 
