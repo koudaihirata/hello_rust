5df303d14a308aa8fa8d7ed3c4564fc23148ce6e
koudaihirata <2230051@ecc.ac.jp>
2024-08-25 13:46:57 +0900

    ジェネリクス

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
-use hello_rust::sample_trait::{double_area, Circle, Rectangle, Shape};
+use std::fmt::Debug;
+
+fn max<T: PartialOrd + Debug>(a: T,b: T) -> T {
+    if a >= b {
+        a
+    } else {
+        b
+    }
+}
 
 fn main() {
-    let rect = Rectangle {
-        width: 4.0,
-        height: 5.0,
-    };
-    let circle = Circle {
-        radius: 2.0,
-    };
-
-    // println!("Rectangle area is: {}", rect.calc_area());
-    // println!("Rectangle perimeter is: {}", rect.calc_perimeter());
-    // Rectangle::do_something();
-    // println!("circle area is: {}", circle.calc_area());
-    // println!("circle perimeter is: {}", circle.calc_perimeter());
-    // Circle::do_something();
-
-    // println!("Rectangle default: {}", rect.default_something());
-    // println!("Circle default: {}", circle.default_something());
-
-    println!("Rectangle double area: {}", double_area(&rect));
-    println!("Circle double area: {}", double_area(&circle));
+    println!("{}", max(1, 2));
+    println!("{}", max(1.1, 1.2));
+    println!("{}", max("x", "a"));
 }
